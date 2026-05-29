100c690eccda4c612dec191ae635b70413cda1af
Arham Athillah <arhamathillah@gmail.com>
2026-05-29 12:33:56 +0700

    clean up root directory and keep only frontend folder

--- src/services/achievementService.js
+++
@@ -1,7 +0,0 @@
-import { apiFetch, authHeader } from './api';
-
-export const getMyAchievements = async () => {
-  return apiFetch('/achievements/my-badges', {
-    headers: authHeader(),
-  });
-};
